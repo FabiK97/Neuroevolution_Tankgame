1c733ee20826857383686bef654f2447790d8d05
FabiK97 <1710653857@stud.fh-kufstein.ac.at>
2019-10-10 16:51:39 +0100

    added tankturret class

--- tank.js
+++ tank.js
@@ -4,6 +4,8 @@ class Tank {
         this.vel = createVector();
         this.orientation = dir;
         this.rotation = 0;
+
+        this.turret = new TankTurret(this.pos.x, this.pos.y, this.orientation);
     }
 
     handleInputs() {
@@ -11,40 +13,54 @@ class Tank {
         if(keyIsDown(UP_ARROW)) {
 
             this.vel = p5.Vector.fromAngle(this.orientation);
-            this.vel.setMag(0.5); 
+            this.vel.setMag(0.2); 
 
         } else if(keyIsDown(DOWN_ARROW)) {
 
             this.vel = p5.Vector.fromAngle(this.orientation + Math.PI);
-            this.vel.setMag(0.5);  
+            this.vel.setMag(0.2);  
         }
         
         if(keyIsDown(RIGHT_ARROW)) {
-            this.rotation += 0.005;
+            this.rotation += 0.002;
         }
         
         if(keyIsDown(LEFT_ARROW)) {
-            this.rotation -= 0.005;                
+            this.rotation -= 0.002;                
+        }
+
+        //turn Turret
+        if(keyIsDown(68)) {
+            this.turret.rotate(0.002);
+        }
+        
+        if(keyIsDown(65)) {
+            this.turret.rotate(-0.002);                        
         }
     }
 
     update(dt) {
         this.handleInputs();   
-             
+
         this.pos.add(this.vel.mult(dt));
         this.orientation += this.rotation * dt;
+
         this.rotation = 0;
         this.vel.mult(0);
+        
+        this.turret.update(this.pos.x, this.pos.y, dt);
     }
 
     show() {
         push();
-        noStroke();
-        fill(255);
-        translate(this.pos.x, this.pos.y);
-        rotate(this.orientation);
-        rectMode(CENTER);
-        rect(0,0,60,50);
+            noStroke();
+            fill(255);
+            translate(this.pos.x, this.pos.y);
+            rotate(this.orientation);
+            rectMode(CENTER);
+            rect(0,0,60,50);
         pop();
+
+        this.turret.show();
     }
 }
---
+++ tankturret.js
@@ -0,0 +1,31 @@
+class TankTurret {
+    constructor(x,y,dir) {
+        this.pos = createVector(x,y);
+        this.orientation = dir;
+        this.rotation = 0;
+    }
+    
+    rotate(angle) {
+        this.rotation = angle;
+    }
+
+    update(x,y,dt) {
+        this.pos.x = x;
+        this.pos.y = y;
+
+        this.orientation += this.rotation * dt;
+        this.rotation = 0;
+    }
+
+    show() {
+        push();
+            noStroke();
+            fill(150);
+            translate(this.pos.x, this.pos.y);
+            rotate(this.orientation);
+            ellipse(0, 0, 30,30);
+            fill(100)
+            rect(-5,-35, 10, 40);
+        pop();
+    }
+}

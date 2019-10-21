73633509dda4fb15174ffb84de3f9cd0a95868f0
FabiK97 <1710653857@stud.fh-kufstein.ac.at>
2019-10-21 22:05:52 +0100

    added biases and copy method to Neural Network

--- NeuralNetwork/neuralnetwork.js
+++ NeuralNetwork/neuralnetwork.js
@@ -24,10 +24,16 @@ class NeuralNetwork {
         this.weights_i_h1 = this.weights_i_h1.map(randomize);
         this.weights_h1_h2 = this.weights_h1_h2.map(randomize);
         this.weights_h2_o = this.weights_h2_o.map(randomize);
-        console.table(this.weights_i_h1._data);
-        console.table(this.weights_h1_h2._data);
-        console.table(this.weights_h2_o._data);
+        
+        //create biases
 
+        this.bias_h1 = math.zeros(this.hn_1);
+        this.bias_h2 = math.zeros(this.hn_2);
+        this.bias_o = math.zeros(this.on);
+
+        this.bias_h1 = this.bias_h1.map(randomize);
+        this.bias_h2 = this.bias_h2.map(randomize);
+        this.bias_o = this.bias_o.map(randomize);
     }
 
 
@@ -41,19 +47,34 @@ class NeuralNetwork {
 
         //calculate signals into first hidden layer
         let h1_inputs = math.multiply(inputs, this.weights_i_h1);
+            console.log(h1_inputs.size());
+            console.log(this.bias_h1.size());
+            h1_inputs = math.add(h1_inputs, this.bias_h1);
         let h1_outputs = h1_inputs.map(sigmoid);
 
         //calculate signals into second hidden layer
         let h2_inputs = math.multiply(h1_outputs, this.weights_h1_h2);
+            h2_inputs = math.add(h2_inputs, this.bias_h2);
         let h2_outputs = h2_inputs.map(sigmoid);
 
         //calculate signals into final output layer
         let final_inputs = math.multiply(h2_outputs, this.weights_h2_o);
+            final_inputs = math.add(final_inputs, this.bias_o);
         let final_outputs = final_inputs.map(sigmoid);
         
+        //return Array of outputs
         return final_outputs._data;
     }
+    
+    copy() {
+        let copy = new NeuralNetwork(this.in, this.hn_1, this.hn_2, this.on);
+        copy.weights_i_h1 = this.weights_i_h1.clone();
+        copy.weights_h1_h2 = this.weights_h1_h2.clone();
+        copy.weights_h2_o = this.weights_h2_o.clone();
+        return copy;
+    }
 }
+
 
 function sigmoid(x) {
     return 1 / (1 + math.pow(Math.E, -x));
